1642846f8b7d449a058a7cd334eae2b923722c98
mounicamanthena <129820525+mounicamanthena@users.noreply.github.com>
2023-04-09 14:29:46 +0000

    Added Connection to db

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1 ,
-    'title': "FrondEnd Engineer",
-    'location': "Melbourne, VIC",
-    'salary': "$ 100,000",
-  },
-  {
-    'id': 2,
-    'title': "BackEnd Engineer",
-    'location': "Brisbane, QLD",
-    
-  },
-  {
-    'id': 3,
-    'title': "Data Analyst",
-    'location': "Sydney, NSW",
-    'salary': "$ 150,000",
-  },
-  {
-    'id': 4,
-    'title': "Data Scientist",
-    'location': "Remote",
-    'salary': "$ 180,000",
-  }
-]
-
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs = JOBS, company_name = 'R&D')
+  jobs_list = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs_list, company_name='R&D')
 
 @app.route("/api/jobs")
 def hello_r_d():
-  return jsonify(JOBS)
+  jobs_list = load_jobs_from_db()
+  return jsonify(jobs_list)
 
-#print(__name__)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
 
-if __name__ == "__main__" :
-    app.run(host='0.0.0.0', debug=True)
-#print("Hello World!!")
