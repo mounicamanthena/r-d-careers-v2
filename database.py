517d555c4a7ea88133ede73c8543bc3a614d62d4
mounicamanthena <129820525+mounicamanthena@users.noreply.github.com>
2023-04-10 09:43:32 +0000

    Added Application form and acknowledgement displayed

--- database.py
+++ database.py
@@ -26,3 +26,17 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+
+
+#def add_application_form_to_db(job_id, app_form):
+#  with engine.connect() as conn:
+#    print(job_id, app_form['full_name'], app_form['email'])
+#    print(type(job_id))
+#    result = conn.execute(
+ #     text(
+  #      f"INSERT INTO 'randdcareers'.'applicationforms'  (job_id, full_name, email,linkedin_url,education, work_experience, reume_url) VALUES(:{job_id},:{app_form['full_name']},:{app_form['email']},:{app_form['linkedin_url']},:{app_form['education']},:{app_form['work_experience']},:{app_form['reume_url']})"
+   #   ))
+    #rows = result.all()
+    #print(rows[0]._asdict())
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
@@ -22,6 +22,17 @@ def show_job(id):
   if not job:
     return "Not Found 404"
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  app_form = request.form
+  job = load_job_from_db(id)
+  # job_id = int(id)
+  # print(job['id'])
+  # add_application_form_to_db(job['id'], app_form)
+  #print(1)
+  return render_template('app_form_submitted.html', app_form=app_form, job=job)
 
 
 if __name__ == "__main__":
